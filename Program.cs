f1278fbe23a3b3dea2dc0e99024f32bdc33982f8
[mahapatrasonali] <[sonangr2000@gmail.com]>
2022-12-28 20:05:56 +0530

    UC2

--- Program.cs
+++ Program.cs
@@ -7,9 +7,10 @@ namespace LinkedListDemo
 
         public static void Main(String[] args)
         {
-            Console.WriteLine("Welcome To ");
+            Console.WriteLine("Welcome To LinkedList Program ");
             Console.WriteLine("Choose a option from below\n" +
-                "Choose 1): Creating a linked list ");
+                "Choose 1): Creating a linked list \n " +
+                "\nChoose 2) : Creating a linked list \n ");
             int option = Convert.ToInt32(Console.ReadLine());
 
             switch (option)
@@ -17,11 +18,19 @@ namespace LinkedListDemo
                 case 1:
                     LinkedList linkedList = new LinkedList();
                     linkedList.Add(56);
-                    linkedList.Add(30);
+                    
                     linkedList.Add(30);
                     linkedList.Add(70);
                     linkedList.Display();
                     break;
+                case 2:
+                    linkedList = new LinkedList();
+                    linkedList.AddFirst(70);
+                    linkedList.AddFirst(30);
+                    linkedList.AddFirst(56);
+                    linkedList.Display();
+                    break;
+
                 default:
                     Console.WriteLine("Choose a Valid Option");
                     break;
